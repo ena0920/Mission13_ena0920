c87bcfdcd8d37dfa0865ccd2796e296870235af6
Emma Antonucci <70171050+ena0920@users.noreply.github.com>
2023-03-31 15:53:44 -0600

    Add files via upload

---
+++ src/MovieCollection.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MovieList from './Movies';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    <MovieList />
+  </React.StrictMode>,
+);
+
+function TopBanner(props: any) {
+  return (
+    <>
+      <TopBanner saying="Joel Hilton's Movie Mania - Movie Collection" />
+      <div className="row">
+        <div className="col-3">
+          <img
+            src="./JoelHiltonHeadshot.jpg"
+            alt="Joel Hilton"
+            width="250px"
+            height="150px"
+          ></img>
+        </div>
+
+        <div className="col align-self-center text-start">
+          <h1>{props.saying}</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TopBanner;
